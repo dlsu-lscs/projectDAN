48ed625991d7999acabf9b5e3d68b19534db6983
Carlos <carlos_ngo@dlsu.edu.ph>
2018-11-02 18:33:22 +0800

    Done setting up orgres container

--- src/smart-components/orgres-container/orgres-container.js
+++ src/smart-components/orgres-container/orgres-container.js
@@ -11,57 +11,38 @@ import { _setInputState } from '../../mixins/InputHandlerMixin';
 
 import { auth } from '../../utils/firebase';
 
+import { template } from './template';
+
+import { documents } from '../../utils/databse-sample';
+
 class OrgresContainer extends Component {
     constructor(){
         super();
         this.state = {
-            email: "",
-            password: ""
+            found = false,
+            document: {}
         }
-        this.login = this.login.bind(this);
+    }
+    componentDidMount(){
+        let { id } = this.props;
+        if( documents[id] != null){
+            this.setState({found: true});
+            this.setState({document: documents[id]});
+        }
+        console.log(documents);
     }
     render() {
-        let { history } = this.props;
-        return (
-        <div className="page-login">
-            <div className = "formContainer">
-                <form onSubmit = {this.login}>
-                    <InputComponent 
-                        parent = { this }
-                        name = "email"
-                        value = { this.state.email }
-                        type = "text" 
-                        _setInputState = { _setInputState }
-                    >
-                    </InputComponent>
-                    <InputComponent 
-                        parent = { this }
-                        name = "password"
-                        value = { this.state.password }
-                        type = "password" 
-                        _setInputState = { _setInputState }
-                    >
-                    </InputComponent>
-                    <button type = "submit" className = "loginButton">Login</button>
-                    <button type = "button" onClick = {() => {history.push('/signup')}} className = "loginButton" >No account yet?</button>
-                </form>
-            </div>
-        </div>
-        );
-    }
-    login(event){
-        event.preventDefault();
-        console.log("Logged in as: \n"+this.state.email+"\n"+this.state.password);
-        auth.signInWithEmailAndPassword(this.state.email, this.state.password).catch(function(error) {
-        // Handle Errors here.
-        console.warn(error);
-        // ...
-        });
+        if(!this.state.found ){
+            return(
+                <div>Document not found</div>
+            )
+            return (template(this));
+        }
     }
 }
 
-LoginContainer.propTypes = {
+OrgresContainer.propTypes = {
     history: PropTypes.object.isRequired
 };
 
-export default withRouter(LoginContainer);
+export default withRouter(OrgresContainer);
