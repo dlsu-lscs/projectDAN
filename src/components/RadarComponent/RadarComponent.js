02a768940830ed04c708104da1e4e1006a4b16d6
JoshCrusader <joshua_cruzada@dlsu.edu.ph>
2018-12-14 16:53:20 +0800

    Working on radar data

--- src/components/RadarComponent/RadarComponent.js
+++ src/components/RadarComponent/RadarComponent.js
@@ -4,7 +4,58 @@ import {Radar} from 'react-chartjs-2';
 class RadarComponent extends React.Component {
     
     constructor(props) {
-        super(props)   
+        super(props);
+        this.state = {
+            data: {
+                labels: ['1', '2', '3', '4', '5'],
+                datasets: [{
+                    label: "Question 1",
+                    backgroundColor: "rgba(255,0,0,0.2)",
+                    borderColor: "rgba(255,0,0,1)",
+                    pointBackgroundColor: "rgba(255,0,0,1)",
+                    pointBorderColor: "#fff",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(255,0,0,1)",
+                    data: []
+                }, {
+                    label: "Question 2",
+                    backgroundColor: "rgba(255,255,0,0.2)",
+                    borderColor: "rgba(255,255,0,1)",
+                    pointBackgroundColor: "rgba(255,255,0,1)",
+                    pointBorderColor: "#fff",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(255,255,0,1)",
+                    data: []
+                }, {
+                    label: "Question 3",
+                    backgroundColor: "rgba(0,0,255,0.2)",
+                    borderColor: "rgba(0,0,255,1)",
+                    pointBackgroundColor: "rgba(0,0,255,1)",
+                    pointBorderColor: "#fff",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(0,0,255,1)",
+                    data: []
+                }, {
+                    label: "Question 4",
+                    backgroundColor: "rgba(0,255,0,0.2)",
+                    borderColor: "rgba(0,255,0,1)",
+                    pointBackgroundColor: "rgba(0,255,0,1)",
+                    pointBorderColor: "#fff",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(0,255,0,1)",
+                    data: []
+                }, {
+                    label: "Question 5",
+                    backgroundColor: "rgba(0,255,255,0.2)",
+                    borderColor: "rgba(0,255,255,1)",
+                    pointBackgroundColor: "rgba(0,255,255,1)",
+                    pointBorderColor: "#fff",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(0,255,255,1)",
+                    data: []
+                },]
+            }
+        }
         this.fetchData = this.fetchData.bind(this)
     }
 
@@ -12,64 +63,16 @@ class RadarComponent extends React.Component {
         let docu = this.props.document
         let results = [];
         var field;
-        var mostRecent = docu.OrgresDetails[docu.OrgresDetails.length - 1]
-        // console.log(mostRecent);
-        for (field in mostRecent) {
-            let row = []
-            for (let i = 0; i < mostRecent[field].length; i++) {
-                row.push(mostRecent[field][i])
-            }
-            results.push(row);
-        }
-        var data = {
-            labels: ['1', '2', '3', '4', '5'],
-            datasets: [{
-                label: "Question 1",
-                backgroundColor: "rgba(255,0,0,0.2)",
-                borderColor: "rgba(255,0,0,1)",
-                pointBackgroundColor: "rgba(255,0,0,1)",
-                pointBorderColor: "#fff",
-                pointHoverBackgroundColor: "#fff",
-                pointHoverBorderColor: "rgba(255,0,0,1)",
-                data: results[0]
-            }, {
-                label: "Question 2",
-                backgroundColor: "rgba(255,255,0,0.2)",
-                borderColor: "rgba(255,255,0,1)",
-                pointBackgroundColor: "rgba(255,255,0,1)",
-                pointBorderColor: "#fff",
-                pointHoverBackgroundColor: "#fff",
-                pointHoverBorderColor: "rgba(255,255,0,1)",
-                data: results[1]
-            }, {
-                label: "Question 3",
-                backgroundColor: "rgba(0,0,255,0.2)",
-                borderColor: "rgba(0,0,255,1)",
-                pointBackgroundColor: "rgba(0,0,255,1)",
-                pointBorderColor: "#fff",
-                pointHoverBackgroundColor: "#fff",
-                pointHoverBorderColor: "rgba(0,0,255,1)",
-                data: results[2]
-            }, {
-                label: "Question 4",
-                backgroundColor: "rgba(0,255,0,0.2)",
-                borderColor: "rgba(0,255,0,1)",
-                pointBackgroundColor: "rgba(0,255,0,1)",
-                pointBorderColor: "#fff",
-                pointHoverBackgroundColor: "#fff",
-                pointHoverBorderColor: "rgba(0,255,0,1)",
-                data: results[3]
-            }, {
-                label: "Question 5",
-                backgroundColor: "rgba(0,255,255,0.2)",
-                borderColor: "rgba(0,255,255,1)",
-                pointBackgroundColor: "rgba(0,255,255,1)",
-                pointBorderColor: "#fff",
-                pointHoverBackgroundColor: "#fff",
-                pointHoverBorderColor: "rgba(0,255,255,1)",
-                data: results[4]
-            },]
-        }
+        // var mostRecent = docu.OrgresDetails[docu.OrgresDetails.length - 1]
+        // // console.log(mostRecent);
+        // for (field in mostRecent) {
+        //     let row = []
+        //     for (let i = 0; i < mostRecent[field].length; i++) {
+        //         row.push(mostRecent[field][i])
+        //     }
+        //     results.push(row);
+        // }
+        let data = this.state.data;
         return data
     }
     
